a2c055a8286dbe35bdd34862a63da01de6408adb
Hactually Benji <mrbenamiller@gmail.com>
2023-11-04 10:39:28 -0700

    Simple single page does not need routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import ProgressiveImage from './components/ProgressiveImage';
@@ -6,16 +5,7 @@ import ProgressiveImage from './components/ProgressiveImage';
 function App() {
   return (
     <>
-    <Routes>
-      <Route path="/home" element={ <Header /> } />
-      <Route path="/customimage" element={
-        <ProgressiveImage
-          src={require('./images/Tortellini.png')}
-          placeholderSrc="./images/Small_Tortellini.png"
-          width="700"
-          height="700"
-        /> } />
-    </Routes>
+      <Header />
     </>
   );
 }
